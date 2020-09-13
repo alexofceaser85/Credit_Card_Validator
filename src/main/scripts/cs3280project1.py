6966678bc35e763eb549b33fa3acd1aa029dc7f9
alex decesare <alexdecesare@hotmail.com>
2020-09-12 21:25:26 -0400

    Added the main method of the program, this method well be responsible for calling the methods in the utils files. Currently the method holds the information from the credit_card_types.ssv file and finds the credit card numbers of equal length to the one inputted

---
+++ src/main/scripts/cs3280project1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+This is the main class for the program, it accepts a location for a file containing card data and determines if the
+card is of the correct format, the card issuer, and if the card is authentic
+"""
+import sys
+sys.path.append('../python')
+import utils
+import os
+from pathlib import Path
+
+__author__ = "Alex DeCesare"
+__version__ = "12-September-2020"
+
+file_location = Path('../../../docs/credit_card_types.ssv')
+credit_card_list = []
+
+def main():
+    """
+    This is the main method of the program, calls the methods to determine if a card is the correct format, the card     issuer, and if the card is authentic
+    """
+    print('Please enter a credit card number')
+    credit_card_number = input()
+
+    parse_credit_card_file()
+
+def parse_credit_card_file():
+
+    """
+    parses the cards in the credit card types file and retrives the required information and adds the new card
+    to the credit card list variable
+    """
+
+    card_file = open(file_location, 'r')
+    card_file_contents = card_file.readlines()
+
+    for current_card in card_file_contents:
+        current_card_properties = current_card.split(';')
+        
+        credit_card = {
+                'card_issuer' : current_card_properties[0],
+                'card_length' : current_card_properties[1].split(','),
+                'card_start_digits' : remove_new_line_tag(current_card_properties[2].split(','))
+                }
+
+        credit_card_list.append(credit_card)
+
+def remove_new_line_tag(card_start_digits):
+    
+    formatted_card_start_digits = []
+
+    for start_digit in card_start_digits:
+        
+        if '\n' in start_digit:
+            remove_new_line = start_digit.replace('\n', '')
+            formatted_card_start_digits.append(remove_new_line)
+        else:
+            formatted_card_start_digits.append(start_digit)
+    return formatted_card_start_digits
+
+if __name__ == '__main__':
+    main()
